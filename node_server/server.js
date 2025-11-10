a65396c541d675e27e111d77152d7e98ca4c32dc
amadoood <amadod@bu.edu>
2025-11-10 09:56:20 -0500

    Added websocket and webhook to server component

--- node_server/server.js
+++ node_server/server.js
@@ -1,15 +1,44 @@
-const express = require('express');
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
 const app = express();
 const PORT = 3000;
 
+MOCK_ORDERS = []
+
 app.use(express.json());
 
-//Endpoints go here
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log("Frontend connected: ", socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('Frontend disconnected: ', socket.id);
+    });
+});
+
 app.get('/', (req, res) => {
     res.send("Mug Exchange server!");
 });
 
+app.post('/api/grubhub/webhook', (req, res) => {
+    console.log("Webhook received", req.body);
 
-app.listen(PORT, () => {
+    MOCK_ORDERS.push(req.body);
+
+    io.emit("Order update", req.body);
+
+    res.status(200).json({message: "Webhook received"})
+});
+
+
+server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
--- tests/grubhub_sim.py
+++ tests/grubhub_sim.py
@@ -1,30 +1,17 @@
-from flask import Flask, request, jsonify
 from datetime import datetime, timedelta
-import threading
 import time
+import requests
 
-app = Flask(__name__)
+WEBHOOK_URL = "http://localhost:3000/api/grubhub/webhook"
 
 # Mock data to simulate "orders" from Grubhub
-MOCK_ORDERS = [
-    {
-        "orderId": "1000",
-        "merchantId": "1",
-        "status": "IN_PROGRESS",
-        "orderDate": "2025-10-30T00:00:00Z",
-        "items": [
-            {"name": "Burger", "quantity": 2, "price": 9.99},
-            {"name": "Fries", "quantity": 1, "price": 3.49}
-        ],
-        "total": 23.47
-    }
-]
+MOCK_ORDERS = []
 
 def generate_mock_orders():
     order_counter = 1001
     time_counter = "2025-10-30T00:00:05Z"
 
-    time.sleep(10)
+    #time.sleep(10)
     while True:
         time.sleep(5)
 
@@ -47,28 +34,13 @@ def generate_mock_orders():
         new_time = datetime.strptime(time_counter, "%Y-%m-%dT%H:%M:%SZ") + timedelta(seconds = 5)
         time_counter = new_time.strftime("%Y-%m-%dT%H:%M:%SZ")
 
-@app.route("/pos/orders", methods=["GET"])
-def get_orders():
-    merchant_id = request.args.get("merchant_long_id")
-    status = request.args.get("status")
-    start_date = request.args.get("startDate")
-    end_date = request.args.get("endDate")
+        try:
+            response = requests.post(WEBHOOK_URL, json=new_order)
+            print(f"Sent order to node.js server with status {response.status_code}")
+        except Exception as e:
+            print(f"Failed sending order via webook: {e}")
 
-    filtered_orders = []
 
-    for order in MOCK_ORDERS:
-        order_date = datetime.strptime(order["orderDate"], "%Y-%m-%dT%H:%M:%SZ")
-        start = datetime.strptime(start_date, "%Y-%m-%dT%H:%M:%SZ")
-        end = datetime.strptime(end_date, "%Y-%m-%dT%H:%M:%SZ")
-
-        if (start <= order_date <= end):
-            filtered_orders.append(order)
-    
-    return jsonify({
-        "orders": filtered_orders
-    })
 
 if __name__ == "__main__":
-    threading.thread(target=generate_mock_orders, daemon=True).start()
-
-    app.run(host="localhost", port = 5000, debug=True)
+    generate_mock_orders()
